a89a76996c9453daa51c3e5a0348e8aacbb079a7
徐志强 <652732310@qq.com>
2021-04-30 10:29:21 +0800

    no sleep when handleLockDepositEvents

--- manager/bscmanager.go
+++ manager/bscmanager.go
@@ -432,7 +432,7 @@ func (this *BSCManager) handleLockDepositEvents(refHeight uint64) error {
 		return fmt.Errorf("handleLockDepositEvents - this.db.GetAllRetry error: %s", err)
 	}
 	for _, v := range retryList {
-		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Second * 1)
 		crosstx := new(CrossTransfer)
 		err := crosstx.Deserialization(common.NewZeroCopySource(v))
 		if err != nil {
